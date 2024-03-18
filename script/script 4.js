1bf2534d410842c3b30271e89b158fa2e76c0885
meraj khalili <meraj.khalili@gmail.com>
2024-03-19 00:27:29 +0330

    first-commit

---
+++ script/script 4.js
@@ -0,0 +1,38 @@
+// submit 
+
+/*
+
+
+let btn = document.querySelector(".form")
+
+btn.addEventListener("submit" , function() {
+    console.log("submit shod :)")
+})
+
+*/
+
+// event 
+
+/*
+
+let logBtn = document.querySelector(".btn")
+
+logBtn.addEventListener("click" , function(event) {
+
+    console.log (event)
+})
+
+*/
+
+// node 
+
+/*
+
+let h1Elem = document.querySelector(".title")
+
+h1Elem.addEventListener("dblclick" , function(){
+    console.log(h1Elem.nodeName)
+})
+
+*/
+
